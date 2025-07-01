ca48aa868304ef26756098d6fa9bef559047393e
ahn-sj <jaesa5221@gmail.com>
2025-07-01 14:11:13 +0900

    Initial Commit

---
+++ src/main/java/tally/notification/SimpleNotificationApplication.java
@@ -0,0 +1,13 @@
+package tally.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tally/notification/SimpleNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package tally.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
