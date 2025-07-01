d84ac4a267cf1ae0167e2a44b39e8abd76415d23
ahn-sj <jaesa5221@gmail.com>
2025-07-01 15:19:57 +0900

    Initial Commit

---
+++ config-server/src/test/java/tally/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tally.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
